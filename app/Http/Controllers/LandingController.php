8942082c311ff6946cc74682f8b9d7fa1366eb0f
Maradho <maradho@gmail.com>
2023-12-03 23:16:32 +0700

    first commit

---
+++ app/Http/Controllers/LandingController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\ProfileUpdateRequest;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\View\View;
+
+class LandingController extends Controller
+{
+
+    public function get(Request $request)
+    {
+        return view('landing');
+    }
+
+}
---
+++ database/migrations/2023_12_03_000000_create_informasi.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('informasi', function (Blueprint $table) {
+            $table->id();
+            $table->integer('id_user');
+            $table->integer('id_kategori');
+            $table->string('judul');
+            $table->text('deskripsi')->nullable();
+            $table->string('gambar')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('informasi');
+    }
+};
